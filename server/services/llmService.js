042462a7c7fae6ae735ada61a5b0251011c73a32
Sameer Pathan <pathansameer67421@gmail.com>
2026-02-24 08:45:10 +0530

    revoked ollama and added groq api for efficient process

--- server/services/llmService.js
+++ server/services/llmService.js
@@ -1,25 +1,31 @@
-const axios = require("axios");
+const Groq = require("groq-sdk");
+
+const groq = new Groq({
+  apiKey: process.env.GROQ_API_KEY,
+});
 
 async function generateAnswer(context, question) {
-  const response = await axios.post(
-    "http://localhost:11434/api/generate",
-    {
-      model: "llama3:latest",
-      prompt: `
-Answer ONLY using the context below.
-If answer not found, say "Not in document."
+  const prompt = `
+You are a helpful assistant.
+Answer ONLY from the provided context.
+If answer is not found in context, say "Not found in document."
 
 Context:
 ${context}
 
 Question:
 ${question}
-`,
-      stream: false
-    }
-  );
+`;
 
-  return response.data.response;
+  const completion = await groq.chat.completions.create({
+    model: "llama-3.1-8b-instant", 
+    messages: [
+      { role: "user", content: prompt }
+    ],
+    temperature: 0.2
+  });
+
+  return completion.choices[0].message.content;
 }
 
 module.exports = generateAnswer;
